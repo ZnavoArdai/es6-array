c9e87f5584865174f416b5343ea93b98cb1c8589
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-06-18 16:04:37 +0300

    commit

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-var imgArray = ['https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg',
+let companyArray=["goodle","facebook","intel","apple","at&t","brrb","sdfsdf"]
+
+companyArray.forEach(i => {
+    printcompany.innerText+=i+"\n";
+});
+
+let imgArray = ['https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg',
 "https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
 'https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg',
 'https://thumbs.dreamstime.com/b/rainbow-love-heart-background-red-wood-60045149.jpg',
@@ -24,12 +30,40 @@ for (let i = 0; i < imgArray.length; i++) {
 
 
 
+let person={
+    company:"bbrre",
+    city:"jerosalem",
+    img:"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
+    id:"compi",
+    fild:"since",
+    link:"www.google.com"
 
+
+}
 let obArray=[person,person,person,person,person,person,person,person,person,person]
 
 
-for (let i = 0; i < imgArray.length; i++) {
+for (let i = 0; i < obArray.length; i++) {
     div2.innerHTML+="<div>"+obArray[i].company+" "+obArray[i].city+" "+"<br>"+"<img style='height:100px;width:100px' src="+obArray[i].img+">"+"</div>"
+            let divs=document.getElementsByTagName("div");
+
+    for (const key of divs) {
+if(key.id=="")
+        key.id=person.id+i
+                    
+                }
+
+
+}
+
+let img=document.getElementsByTagName("img");
+
+for (const key of img) {
+
+key.onclick=(()=>{
+
+    window.location.href="https://en.wikipedia.org/wiki/Vikings_(TV_series)"
+})
     
 }
 
@@ -40,11 +74,11 @@ for (let i = 0; i < imgArray.length; i++) {
 
 
 
-var person={
-    company:"bbrre",
-    city:"jerosalem",
-    img:"https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
 
-}
+
+
+
+
+
 
 
